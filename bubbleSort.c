09acda41be27f8927bae9df18cb2a6a358eb2bd9
gothicsky <69666943+gothicsky@users.noreply.github.com>
2026-04-24 15:26:24 +0300

    Refactor bubble sort to include safe version
    Changed bubbleSort from main function to seperate function. Refactored bubble sort implementation to include a safe version that returns a new sorted array. Updated main function to demonstrate both sorting methods.

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,30 +1,73 @@
 #include <stdio.h>
-// #include <stdlib.h>
-// #include <time.h>
+#include <stdlib.h>
 
 #define N 10
 
-int main() {
+int* bubble_sort_safe(int arr[], int arrSize) {
 
-    int A[N]={5,1,3,4,10,6,23,12,44,13};
+    int* sortedArray = (int *)malloc(arrSize * sizeof(int));
+
+    if (sortedArray==NULL) {
+        exit(1);
+    }
+
+    for (int i=0; i<arrSize; i++) {
+        sortedArray[i]=arr[i];
+    }
+
+
     int swapped;
-
     do {
         swapped = 0;
-        for (int i =1, temp; i<N; i++) {
-            if (A[i-1]>A[i]) {
-                temp = A[i-1];
-                A[i-1]=A[i];
-                A[i]=temp;
+        for (int i =1, temp; i<arrSize; i++) {
+            if (sortedArray[i-1]>sortedArray[i]) {
+                temp = sortedArray[i-1];
+                sortedArray[i-1]=sortedArray[i];
+                sortedArray[i]=temp;
                 swapped=1;
             }
         }
 
     }while (swapped);
 
-    for (int i = 0; i < 10; ++i) {
-        printf("%d ", A[i]);
-    }
+    return sortedArray;
+}
+
+void bubble_sort(int *arr, int arrSize) {
+    int swapped;
+    do {
+        swapped = 0;
+        for (int i =1, temp; i<arrSize; i++) {
+            if (arr[i-1]>arr[i]) {
+                temp = arr[i-1];
+                arr[i-1]=arr[i];
+                arr[i]=temp;
+                swapped=1;
+            }
+        }
+
+    }while (swapped);
+}
+
+int main() {
+
+    int A[N]={5,1,3,4,10,6,23,12,44,13};
+
+    //returns a new array
+    // int* B = bubble_sort_safe(A, N);
+    // for (int i = 0; i < 10; ++i) {
+    //     printf("%d ", B[i]);
+    // }
+    // free(B);
+
+
+    //changes the array
+    // bubble_sort(A, N);
+    // for (int i = 0; i < 10; ++i) {
+    //     printf("%d ", A[i]);
+    // }
+
+
 
     return 0;
 }
